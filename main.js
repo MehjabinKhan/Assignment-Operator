15567d1a0619e69647f6833c41940df6010561f5
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-12 21:02:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+// Assignment Operators
+let c = 10;
+c += 5;
+console.log(c);
+let xy = 22;
+xy += 2;
+console.log(xy);
+let aliPocketMoney = 200;
+let spend = 120;
+let left = aliPocketMoney - spend;
+left += 23;
+console.log(left);
+let result = 34;
+result -= 4;
+console.log(result);
+let ab = 78;
+let cd = 60;
+let sum = ab + cd;
+sum += 2;
+console.log(sum);
